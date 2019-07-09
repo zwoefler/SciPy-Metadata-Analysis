3d0b317e77522e45aba3d1c6fc62c3074197795a
Martin Fabian <martinfz@t-online.de>
2019-07-09 16:33:43 +0200

    Implemented argparse for buildscript

--- scripts/gather_paper_information.py
+++ scripts/gather_paper_information.py
@@ -1,13 +1,12 @@
 """This module gathers the paper information extracted from the bookmarks"""
+import argparse
 import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-JSON_URL_FILE = "../resources/test_urls.json"
 PARAMETERS_TO_EXTRACT = [
     "title", "author", "journal", "impact factor", "citations", "publishing date"]
 PAPER_INFORMATION_DICT = {}
-EXPORT_JSON_FILE_NAME = "paper_information.json"
 
 
 def read_papers_urls(url_file):
@@ -16,9 +15,9 @@ def read_papers_urls(url_file):
         return json.load(file_object)
 
 
-def write_paper_parameters_to_json(parameter_dict):
+def write_paper_parameters_to_json(parameter_dict, export_file_name):
     """Writes the gatherd paper information dictionaries to a json file"""
-    with open(EXPORT_JSON_FILE_NAME, "w") as file_object:
+    with open(export_file_name, "w") as file_object:
         json.dump(parameter_dict, file_object)
 
 
@@ -91,20 +90,39 @@ def gather_information_from_page(link, driver):
 def main():
     """This is the main function thats gathers the paper
     information based on the extracted booksmarks"""
-    url_list = read_papers_urls(JSON_URL_FILE)
+
+    parser = argparse.ArgumentParser(
+        description='''Returns a JSON file with all the relevant meta data of an URL
+        list with scientific papers from different scientific databases''',
+        usage=''
+    )
+    parser.add_argument(
+        '-u',
+        '--url_list',
+        type=str,
+        help='Path to the URL-JSON file with all the relevant scientific papers',
+        required='True'
+    )
+    parser.add_argument(
+        '-d',
+        '--destination',
+        type=str,
+        help='export destination for all scripts',
+        default='paper_information.json'
+    )
+    args = parser.parse_args()
+    url_json_file = args.url_list
+    export_json_file_name = args.destination
+
+    url_list = read_papers_urls(url_json_file)
     driver = webdriver.Firefox()
     for paper in url_list:
         paper_parameters = gather_information_from_page(paper, driver)
         PAPER_INFORMATION_DICT[paper_parameters["title"]] = paper_parameters
 
-    write_paper_parameters_to_json(PAPER_INFORMATION_DICT)
-    print("Successfully exported the paper information to", EXPORT_JSON_FILE_NAME)
+    write_paper_parameters_to_json(PAPER_INFORMATION_DICT, export_json_file_name)
+    print("Successfully exported the paper information to", export_json_file_name)
     driver.quit()
 
 if __name__ == "__main__":
     main()
-
-
-# +++ TODO +++
-# Implement Argparse for:
-# 1. The link to the file containing the urls of the papers
